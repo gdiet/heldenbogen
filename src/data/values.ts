29afadfd1736bc8dca8f357d40d8655e80450795
georg <gdiet@users.noreply.github.com>
2021-11-24 20:23:37 +0100

    typescript knows private access

--- src/data/values.ts
+++ src/data/values.ts
@@ -1,28 +1,28 @@
 export type Observer = (value: number) => void
 
 export class Values {
-    readonly _values: Map<string, [number, Observer[]]> = new Map()
+    private readonly values: Map<string, [number, Observer[]]> = new Map()
 
-    _getOrInit(key: string): [number, Observer[]] {
-        const value = this._values.get(key)
-        if (value) return value; else { this._values.set(key, [0, []]); return this._getOrInit(key) }
+    private getOrInit(key: string): [number, Observer[]] {
+        const value = this.values.get(key)
+        if (value) return value; else { this.values.set(key, [0, []]); return this.getOrInit(key) }
     }
 
     val(key: string): number {
-        return this._getOrInit(key)[0]
+        return this.getOrInit(key)[0]
     }
 
     set(key: string, value: number): void {
-        const entry = this._getOrInit(key)
+        const entry = this.getOrInit(key)
         entry[0] = value; entry[1].forEach(observer => observer(value))
     }
 
     observe(key: string, observer: Observer): void {
-        const entry = this._getOrInit(key)
+        const entry = this.getOrInit(key)
         entry[1].push(observer); observer(entry[0])
     }
 
     asArray(): Array<[string, number]> {
-        return Array.from(this._values, x => [x[0], x[1][0]])
+        return Array.from(this.values, x => [x[0], x[1][0]])
     }
 }
