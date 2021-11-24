c726ce1046915ece35ab1100b76060d4aff7ada1
georg <gdiet@users.noreply.github.com>
2021-11-24 19:37:06 +0100

    basic Values implementation

--- src/bogen.ts
+++ src/bogen.ts
@@ -1,4 +1,8 @@
 import { Values } from "./data/values.js"
 
-console.log("hallo")
-new Values()
+const vv = new Values
+vv.observe("MU", (value) => { console.log(`observed ${value}`) })
+console.log(vv.val("MU"))
+vv.set("MU", 4)
+console.log(vv._getOrInit("MU")[0])
+console.log(vv.asArray())
--- src/data/values.ts
+++ src/data/values.ts
@@ -1,5 +1,28 @@
+export type Observer = (value: number) => void
+
 export class Values {
-    constructor() {
-        console.log("values constructor")
+    readonly _values: Map<string, [number, Observer[]]> = new Map()
+
+    _getOrInit(key: string): [number, Observer[]] {
+        const value = this._values.get(key)
+        if (value) return value; else { this._values.set(key, [0, []]); return this._getOrInit(key) }
+    }
+
+    val(key: string): number {
+        return this._getOrInit(key)[0]
+    }
+
+    set(key: string, value: number): void {
+        const entry = this._getOrInit(key)
+        entry[0] = value; entry[1].forEach(observer => observer(value))
+    }
+
+    observe(key: string, observer: Observer): void {
+        const entry = this._getOrInit(key)
+        entry[1].push(observer); observer(entry[0])
+    }
+
+    asArray(): Array<[string, number]> {
+        return Array.from(this._values, x => [x[0], x[1][0]])
     }
 }
