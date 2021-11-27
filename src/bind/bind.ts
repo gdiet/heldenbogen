5e2d7502ea95ddffff90f35096f804a4e1c0db77
georg <gdiet@users.noreply.github.com>
2021-11-27 22:21:43 +0100

    proof of concept generate table rows

--- src/bind/bind.ts
+++ src/bind/bind.ts
@@ -2,16 +2,19 @@ import * as data from "../data/data";
 export * from "./load.js"
 export * from "./save.js"
 
+export function bindNumberInputElement(values: data.Values, label: string, input: HTMLInputElement, initialValue: number): void {
+    values.set(label, initialValue)
+    values.observe(label, value => input.value = `${value}`)
+    input.addEventListener("input", _ => {
+        if (input.validity.valid) values.set(label, parseInt(input.value))
+        else input.value = `${values.val(label)}`
+    })
+}
+
 export function bindNumberInput(values: data.Values, label: string, initialValue: number): void {
     const input = document.getElementById(label)
-    if (input instanceof HTMLInputElement) {
-        values.set(label, initialValue)
-        values.observe(label, value => input.value = `${value}`)
-        input.addEventListener("input", _ => {
-            if (input.validity.valid) values.set(label, parseInt(input.value))
-            else input.value = `${values.val(label)}`
-          })
-    } else console.warn(`Can't bind - no HTMLInputElement for ID '${label}'`)
+    if (input instanceof HTMLInputElement) bindNumberInputElement(values, label, input, initialValue)
+    else console.warn(`Can't bind - no HTMLInputElement for ID '${label}'`)
 }
 
 export function setNumberContent(id: string, number: number): void {
--- src/bogen.ts
+++ src/bogen.ts
@@ -8,3 +8,26 @@ dsa5.Grundwerte.forEach(Grundwert => bind.bindNumberInput(Werte, Grundwert, 10))
 dsa5.Grundwerte.forEach(Grundwert => Werte.observe(Grundwert, _ => bind.setNumberContent("GWAP", dsa5.Summe_AP_Grundwerte(Werte))))
 bind.bindSaveButton(Werte)
 bind.bindLoadButton(Werte)
+
+const Körpertalente: [string, string, boolean, number][] = [
+  ["Körperbeherrschung", "GE/GE/KO", true, 4],
+  ["Taschendiebstahl",   "MU/FF/GE", true, 2],
+]
+
+const kt = document.getElementById("Körpertalente")
+if (kt instanceof HTMLTableElement) {
+  Körpertalente.forEach(([Name, Probe, Behinderung, Faktor]) => {
+    const row = kt.insertRow()
+    row.insertCell().textContent = Name
+    row.insertCell().textContent = Probe
+    row.insertCell().textContent = Behinderung ? "Ja": "Nein"
+    row.insertCell().textContent = String.fromCharCode(64 + Faktor)
+    const input = document.createElement("input")
+    input.type = "number"
+    input.value = "0"
+    input.min = "0"
+    input.max = "20"
+    bind.bindNumberInputElement(Werte, Name, input, 0)
+    row.insertCell().appendChild(input)
+  })
+}
